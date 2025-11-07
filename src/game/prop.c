b56ca2db40abc9064193f0bc4b4f7bf6b8433ab5
joshuarwood <joshua.randall.wood@gmail.com>
2025-11-07 15:08:50 -0600

    added flag to ignore line-of-sight hits for transparent background surfaces

--- src/game/prop.c
+++ src/game/prop.c
@@ -983,7 +983,7 @@ struct prop *shotCalculateHits(s32 handnum, bool isshooting, struct coord *gunpo
 bool shotTestLos(struct coord *gunpos2d, struct coord *gundir2d, struct coord *gunpos3d, struct coord *gundir3d, struct coord *endpos3d)
 {
 	struct prop **propptr;
-	struct hitthing sp664;
+	struct hitthing hitthing;
 	struct coord delta;
 	struct shotdata shotdata;
 	s32 i;
@@ -992,6 +992,9 @@ bool shotTestLos(struct coord *gunpos2d, struct coord *gundir2d, struct coord *g
 	RoomNum spb8[8];
 	RoomNum *roomsptr;
 	struct prop *prop;
+
+	// ignore transparent surfaces
+	hitthing.ignore_xlu = 1;
 
 	shotdata.gunpos3d.x = gunpos3d->x;
 	shotdata.gunpos3d.y = gunpos3d->y;
@@ -1040,11 +1043,11 @@ bool shotTestLos(struct coord *gunpos2d, struct coord *gundir2d, struct coord *g
 
 	// Check for BG hits first
 	for (i = 0; rooms[i] != -1; i++) {
-		if (bgTestHitInRoom(&shotdata.gunpos3d, endpos3d, rooms[i], &sp664)) {
+		if (bgTestHitInRoom(&shotdata.gunpos3d, endpos3d, rooms[i], &hitthing)) {
 			// check if it's far enough away from the end point
-			if (fabsf(sp664.pos.x - endpos3d->x) >= 0.1f ||
-					fabsf(sp664.pos.y - endpos3d->y) >= 0.1f ||
-					fabsf(sp664.pos.z - endpos3d->z) >= 0.1f) {
+			if (fabsf(hitthing.pos.x - endpos3d->x) >= 0.1f ||
+					fabsf(hitthing.pos.y - endpos3d->y) >= 0.1f ||
+					fabsf(hitthing.pos.z - endpos3d->z) >= 0.1f) {
 				return false;
 			}
 		}
